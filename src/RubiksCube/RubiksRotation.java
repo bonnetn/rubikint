3b67035782a99f8d6a11af1e10ce0b8480800d1b
Nicolas BONNET <nicolas.bonnet@telecom-sudparis.eu>
2017-01-23 16:21:41 +0100

    Added an enum to represent the moves possible on the cube

---
+++ src/RubiksCube/RubiksRotation.java
@@ -0,0 +1,23 @@
+package RubiksCube;
+
+/*
+ * Definit les mouvements possibles sur le rubiks
+ * U <=> Up
+ * D <=> Down
+ * L <=> Left
+ * R <=> Right
+ * F <=> Front
+ * B <=> Back
+ * 
+ * Ui <=> Up'
+ * etc.
+ */
+
+public enum RubiksRotation {
+	U, Ui,
+	D, Di,
+	R, Ri,
+	L, Li,
+	F, Fi, 
+	B, Bi;
+}
