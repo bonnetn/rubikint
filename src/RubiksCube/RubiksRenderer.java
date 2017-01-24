449411d94b208c7a1389e1d71bc735243c213b34
Nicolas BONNET <nicolas.bonnet@telecom-sudparis.eu>
2017-01-24 09:36:06 +0100

    Removed low level code in RubiksRenderer

--- src/RubiksCube/RubiksRenderer.java
+++ src/RubiksCube/RubiksRenderer.java
@@ -10,10 +10,11 @@ import java.awt.*;
  * Created by shininisan on 23/01/17.
  */
 public class RubiksRenderer {
+	
     private Displayer3D displayer;
     private RubiksCube cube;
-    private BranchGroup parent;
-    private Frame frame;
+    //private BranchGroup parent;
+    //private Frame frame;
     public RubiksRenderer(RubiksCube cube)
     {
         this.cube=cube;
@@ -22,14 +23,29 @@ public class RubiksRenderer {
        // frame = new MainFrame(displayer, 256, 256);
 
     }
+    
+    /**
+     * Retourne un tableau constitué de 9 fois la couleur choisie
+     * @param color 
+     * @return La face colorée uniformément
+     */
+    private Color[] _getColorFace( Color color ) {
+    	Color[] tbl = new Color[9];
+    	for(int i=0; i<9; i++)
+    		tbl[i] = color;
+    	return tbl;
+    	
+    }
+    
     public void afficheCubeFait()
     {
-        Color[] orange={Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE};
-        Color[] red={Color.RED,Color.RED,Color.RED,Color.RED,Color.RED,Color.RED,Color.RED,Color.RED,Color.RED};
-        Color[] white={Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE,Color.WHITE};
-        Color[] green={Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN};
-        Color[] blue={Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE};
-        Color[] yellow={Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW};
+        Color[] orange = _getColorFace(Color.ORANGE);
+        Color[] red    = _getColorFace(Color.RED); 
+        Color[] white  = _getColorFace(Color.WHITE);
+        Color[] green  = _getColorFace(Color.GREEN);
+        Color[] blue   = _getColorFace(Color.BLUE);
+        Color[] yellow = _getColorFace(Color.YELLOW);
+        
         Color[][] colorsToGive={red,white,blue,yellow,green,orange};//FURDLB
         displayer=new Displayer3D(colorsToGive);
 
@@ -39,13 +55,15 @@ public class RubiksRenderer {
     {
         Color[][] toRender=new Color[6][];
         for (int i=0;i<6;i++)
-        {
             toRender[i]=this.cube.getColorFace(RubiksFace.values()[i]);
-        }
+        
         displayer=new Displayer3D(toRender);
     }
+    
     public static void main(String[] args) {
-        RubiksRenderer p=new RubiksRenderer(new RubiksCubeFacetPermutation());
+    
+    	RubiksRenderer p=new RubiksRenderer(new RubiksCubeFacetPermutation());
         p.renderCube();
-        Frame frame = new MainFrame(p.displayer, 256, 256);}
+        //Frame frame = new MainFrame(p.displayer, 256, 256);
+    }
 }
