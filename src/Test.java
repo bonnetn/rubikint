734312bf2f6521375bf612281be3fc582084305d
roxasispoor <manzanoalban@gmail.com>
2017-03-02 12:55:31 +0100

    Suite des procédures faites dans le sens Trigonométrique, le seul qui compte. Pour les copains, les configs ne sont pas faites

--- src/Test.java
+++ src/Test.java
@@ -23,8 +23,8 @@ public class Test {
 		 r.drawItem(rc);
 ArrayList<Procedure> listeProcedure= new ArrayList<Procedure>();
 
-		//Procédure seconde couronne
-		ArrayList<Rotation> rotater=new ArrayList<Rotation>(Arrays.asList(Rotation.B,Rotation.R,Rotation.Bi,Rotation.Ri,Rotation.Fi,Rotation.B,Rotation.F));
+		//Procédure seconde couronne D'R'DRDFD'F'
+		ArrayList<Rotation> rotater=new ArrayList<Rotation>(Arrays.asList(Rotation.D,Rotation.R,Rotation.Di,Rotation.Ri,Rotation.Di,Rotation.Fi,Rotation.D,Rotation.F));
 		RubiksConfiguration conf=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.R))));
 		Procedure p=new Procedure(rotater,10,conf,Rotation.B);
 
@@ -36,9 +36,33 @@ ArrayList<Procedure> listeProcedure= new ArrayList<Procedure>();
             p.translation(Face.values()[Face.F.getValue()+i]);
             p2.translation(Face.values()[Face.F.getValue()+i]);
         }
-		ArrayList<Rotation> croix=new ArrayList<Rotation>(Arrays.asList(Rotation.Fi,Rotation.Li,Rotation.Bi,Rotation.L,Rotation.B,Rotation.F));
+        //croix face à face
+		ArrayList<Rotation> croix=new ArrayList<Rotation>(Arrays.asList(Rotation.Fi,Rotation.Li,Rotation.Di,Rotation.L,Rotation.D,Rotation.F));
 		RubiksConfiguration confcroix=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.D),new FacetConfig(1,0,Face.B,Face.D))));
 		listeProcedure.add(new Procedure(croix,20,confcroix,Rotation.B));
+
+		//croix cote à cote
+		ArrayList<Rotation> croix2=new ArrayList<Rotation>(Arrays.asList(Rotation.Fi,Rotation.Di,Rotation.Li,Rotation.D,Rotation.L,Rotation.F));
+		RubiksConfiguration confcroix2=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.D),new FacetConfig(1,0,Face.L,Face.D))));
+		listeProcedure.add(new Procedure(croix2,20,confcroix2,Rotation.B));
+
+		//chaise
+
+		ArrayList<Rotation> chaise=new ArrayList<Rotation>(Arrays.asList(Rotation.Li,Rotation.D,Rotation.D,Rotation.L,Rotation.D,Rotation.Li,Rotation.D,Rotation.L));
+		RubiksConfiguration confchaise=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.F),new FacetConfig(1,0,Face.R,Face.B),new FacetConfig(1,0,Face.B,Face.L),new FacetConfig(1,0,Face.L,Face.R))));
+		Procedure pchaise=new Procedure(chaise,30,confchaise,Rotation.B);
+		listeProcedure.add(pchaise);
+		Procedure pchaisesym=new Procedure(pchaise);
+		pchaisesym.symetry();
+		listeProcedure.add(pchaisesym);
+
+		//Copain
+		ArrayList<Rotation> copain=new ArrayList<Rotation>(Arrays.asList(Rotation.R,Rotation.Di,Rotation.Li,Rotation.D,Rotation.Ri,Rotation.Di,Rotation.L,Rotation.D));
+		RubiksConfiguration confcopain=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.F),new FacetConfig(1,0,Face.R,Face.B),new FacetConfig(1,0,Face.B,Face.L),new FacetConfig(1,0,Face.L,Face.R))));
+		Procedure pcopain=new Procedure(copain,40,confcopain,Rotation.B);
+		listeProcedure.add(pchaise);
+
+
 
 
 
