5c0eef34775fc7c044a25ea21659712afd1b60e0
roxasispoor <manzanoalban@gmail.com>
2017-03-06 14:08:35 +0100

    coins 1ère couronne

--- src/Test.java
+++ src/Test.java
@@ -22,7 +22,45 @@ public class Test {
 		rc.randomMelange();
 		 r.drawItem(rc);
 ArrayList<Procedure> listeProcedure= new ArrayList<Procedure>();
+		//1ère couronne coins blanc en bas
+		ArrayList<Rotation> coinblanc=new ArrayList<Rotation>(Arrays.asList(Rotation.Fi,Rotation.Di,Rotation.F));
+		RubiksConfiguration confcoinblanc=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(2,0,Face.F,Face.U),new FacetConfig(0,0,Face.D,Face.D))));
+		Procedure procCoin=new Procedure(coinblanc,0,confcoinblanc,Rotation.B);
+		Procedure procCoinsym=new Procedure(procCoin);
+		procCoinsym.symetry();
 
+		for(int i=1;i<=4;i++) {
+			listeProcedure.add(new Procedure(procCoin));
+			listeProcedure.add(new Procedure(procCoinsym));
+			procCoin.translation(Face.values()[Face.F.getValue()+i]);
+			procCoinsym.translation(Face.values()[Face.F.getValue()+i]);
+		}
+	// Procedure coin bloque en dessous
+		ArrayList<Rotation> coinblancbas=new ArrayList<Rotation>(Arrays.asList(Rotation.R,Rotation.Di,Rotation.Di,Rotation.Ri,Rotation.Di,Rotation.R,Rotation.D,Rotation.Ri));
+		RubiksConfiguration confcoinblancbas=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(2,2,Face.D,Face.U),new FacetConfig(0,0,Face.R,Face.F))));
+		Procedure procCoinBas=new Procedure(coinblancbas,5,confcoinblancbas,Rotation.B);
+		Procedure procCoinsymBas=new Procedure(procCoinBas);
+		procCoinsym.symetry();
+
+		for(int i=1;i<=4;i++) {
+			listeProcedure.add(new Procedure(procCoinBas));
+			listeProcedure.add(new Procedure(procCoinsymBas));
+			procCoinBas.translation(Face.values()[Face.F.getValue()+i]);
+			procCoinsymBas.translation(Face.values()[Face.F.getValue()+i]);
+		}
+		//Procedure coin balnc  mal tourné, on le reset juste
+		ArrayList<Rotation> cointourne=new ArrayList<Rotation>(Arrays.asList(Rotation.R,Rotation.D,Rotation.Ri));
+		RubiksConfiguration confcointourne=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(0,2,Face.R,Face.U))));
+		Procedure procCoinTourne=new Procedure(cointourne,5,confcointourne,Rotation.B);
+		Procedure procCoinTournesym=new Procedure(procCoinTourne);
+		procCoinTournesym.symetry();
+
+		for(int i=1;i<=4;i++) {
+			listeProcedure.add(new Procedure(procCoinTourne));
+			listeProcedure.add(new Procedure(procCoinTournesym));
+			procCoinTourne.translation(Face.values()[Face.F.getValue()+i]);
+			procCoinTournesym.translation(Face.values()[Face.F.getValue()+i]);
+		}
 		//Procédure seconde couronne D'R'DRDFD'F'
 		ArrayList<Rotation> rotater=new ArrayList<Rotation>(Arrays.asList(Rotation.D,Rotation.R,Rotation.Di,Rotation.Ri,Rotation.Di,Rotation.Fi,Rotation.D,Rotation.F));
 		RubiksConfiguration conf=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.R))));
