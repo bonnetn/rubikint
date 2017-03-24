6b80f5515882cdca6615ca1d75b82fd59f946a6c
roxasispoor <manzanoalban@gmail.com>
2017-03-24 13:45:53 +0100

    test couronne 2

--- src/TestCouronne1.java
+++ src/TestCouronne1.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-import resolution.BFSSearcher;
-import resolution.Validator;
+import resolution.*;
 import resolution.validators.EdgePositionned;
 import resolution.validators.FirstCrownValidator;
 import rubikscube.RubiksCube;
@@ -64,10 +64,41 @@ public class TestCouronne1 {
 		
 		
 		System.out.println("done.");
-		
-	
-		
-		
-				
+
+		ArrayList<Procedure> listeProcedure= new ArrayList<Procedure>();
+
+		RubiksConfiguration couronne2=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(
+				new FacetConfig(0,1,Face.F,Face.F),new FacetConfig(2,1,Face.F,Face.F),
+				new FacetConfig(0,1,Face.R,Face.R),new FacetConfig(2,1,Face.R,Face.R),
+				new FacetConfig(0,1,Face.B,Face.B),new FacetConfig(2,1,Face.B,Face.B),
+				new FacetConfig(0,1,Face.L,Face.L),new FacetConfig(2,1,Face.L,Face.L)
+		)));
+		ArrayList<Rotation> rotater=new ArrayList<Rotation>(Arrays.asList(Rotation.D,Rotation.R,Rotation.Di,Rotation.Ri,Rotation.Di,Rotation.Fi,Rotation.D,Rotation.F));
+		RubiksConfiguration conf=new RubiksConfiguration(new ArrayList<FacetConfig>(Arrays.asList(new FacetConfig(1,0,Face.F,Face.R))));
+		Procedure p=new Procedure(rotater,10,conf,Rotation.B,couronne2);
+
+		Procedure p2=new Procedure(p);
+		p2.symetry();
+		for(int i=1;i<=4;i++) {
+			listeProcedure.add(new Procedure(p));
+			listeProcedure.add(new Procedure(p2));
+			p.translation(Face.values()[Face.F.getValue()+i]);
+			p2.translation(Face.values()[Face.F.getValue()+i]);
+		}
+		ProceduralSolver proceduralSolver=new ProceduralSolver(rb,10,listeProcedure);
+		proceduralSolver.resolve();
+		System.out.println("U");
+		rb.printFace(Face.U);
+		System.out.println("F");
+		rb.printFace(Face.F);
+		System.out.println("L");
+		rb.printFace(Face.L);
+		System.out.println("R");
+		rb.printFace(Face.R);
+		System.out.println("B");
+		rb.printFace(Face.B);
+
+
+
 	}
 }
