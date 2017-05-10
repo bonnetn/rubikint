ec1c386315319ac0111677d55d0278c67bcc14c1
Florian <eclipse.florian@gmail.com>
2017-05-10 18:00:38 +0000

    reconnaissance de couleur fonctionnel

--- src/ihm/frame/interactivsolver/InteractivSolver_capture.java
+++ src/ihm/frame/interactivsolver/InteractivSolver_capture.java
@@ -3,18 +3,24 @@ package ihm.frame.interactivsolver;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
+import jogamp.nativewindow.windows.PIXELFORMATDESCRIPTOR;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.bytedeco.javacpp.opencv_videoio.*;
 import org.bytedeco.javacv.*;
 import rubikscube.enums.Color;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.RescaleOp;
 import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
 import java.nio.file.Watchable;
 
 import static org.bytedeco.javacpp.opencv_videoio.*;
@@ -29,6 +35,7 @@ public class InteractivSolver_capture extends JLabel{
 
     int faceCapture = 0;
     Color[][] faceColor = new Color[6][9];
+    BufferedImage img;
 
     Webcam webcam;
     WebcamPanel panel;
@@ -105,6 +112,11 @@ public class InteractivSolver_capture extends JLabel{
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Bouton Previous OK");
+                setFace();
+                for (int i=0;i<9;i++)
+                {
+                    System.out.println(faceColor[0][i].getValue());
+                }
 
             }
         });
@@ -112,11 +124,67 @@ public class InteractivSolver_capture extends JLabel{
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Bouton Shoot OK");
-                setFace();
-                for (int i=0;i<9;i++)
-                {
-                    System.out.println(faceColor[0][i].getValue());
-                }
+                //setFace();
+                //for (int i=0;i<9;i++)
+                //{
+                //    System.out.println(faceColor[0][i].getValue());
+                //}
+                /*
+                System.out.println(java.awt.Color.YELLOW);
+                System.out.println(java.awt.Color.WHITE);
+                System.out.println(java.awt.Color.ORANGE);
+                System.out.println(java.awt.Color.RED);
+                System.out.println(java.awt.Color.BLUE);
+                System.out.println(java.awt.Color.GREEN);
+                */
+
+                BufferedImage img = Webcam.getDefault().getImage();
+                float[] rgb = readColor(0,img);
+                System.out.println("Face HG");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(1,img);
+                System.out.println("Face HM");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(2,img);
+                System.out.println("Face HD");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(3,img);
+                System.out.println("Face MG");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(4,img);
+                System.out.println("Face MM");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(5,img);
+                System.out.println("Face MD");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(6,img);
+                System.out.println("Face BG");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(7,img);
+                System.out.println("Face BM");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+                rgb = readColor(8,img);
+                System.out.println("Face BD");
+                System.out.println(rgb[0]);
+                //System.out.println(rgb[1]);
+                //System.out.println(rgb[2]);
+
             }
         });
         webcam = Webcam.getDefault();
@@ -143,16 +211,11 @@ public class InteractivSolver_capture extends JLabel{
     {
         if (faceCapture<6)
         {
-            BufferedImage img = Webcam.getDefault().getImage();
-            faceColor[faceCapture][0] = defineColor(new java.awt.Color(img.getRGB(220,140)));
-            faceColor[faceCapture][1] = defineColor(new java.awt.Color(img.getRGB(320,140)));
-            faceColor[faceCapture][2] = defineColor(new java.awt.Color(img.getRGB(420,140)));
-            faceColor[faceCapture][3] = defineColor(new java.awt.Color(img.getRGB(220,240)));
-            faceColor[faceCapture][4] = defineColor(new java.awt.Color(img.getRGB(320,240)));
-            faceColor[faceCapture][5] = defineColor(new java.awt.Color(img.getRGB(420,240)));
-            faceColor[faceCapture][6] = defineColor(new java.awt.Color(img.getRGB(220,340)));
-            faceColor[faceCapture][7] = defineColor(new java.awt.Color(img.getRGB(320,340)));
-            faceColor[faceCapture][8] = defineColor(new java.awt.Color(img.getRGB(420,340)));
+            img = Webcam.getDefault().getImage();
+            for (int j=0;j<9;j++)
+            {
+                faceColor[faceCapture][j] = defineColor(readColor(j,img));
+            }
             //faceCapture ++;
         }
 
@@ -160,35 +223,70 @@ public class InteractivSolver_capture extends JLabel{
 
     }
 
-
-    public Color defineColor(java.awt.Color color)
+    public float[] readColor(int j, BufferedImage img)
     {
-        int R=color.getRed();
-        int G=color.getGreen();
-        int B=color.getBlue();
-        if (R >= 100 && G <= 50 && B <= 50)
+        java.awt.Color PixelColor;
+        int x=0; int y=0; int R=0; int G=0; int B=0;
+        if (j==0) {x = 215; y = 135;}
+        if (j==1) {x = 315; y = 135;}
+        if (j==2) {x = 415; y = 135;}
+        if (j==3) {x = 215; y = 235;}
+        if (j==4) {x = 315; y = 235;}
+        if (j==5) {x = 415; y = 235;}
+        if (j==6) {x = 215; y = 335;}
+        if (j==7) {x = 315; y = 335;}
+        if (j==8) {x = 415; y = 335;}
+
+        for (int h=0;h<5;h++)
+        {
+            for (int l=0;l<5;l++)
+            {
+               PixelColor = new java.awt.Color(img.getRGB(x+l,y+h));
+               R+=PixelColor.getRed();
+               G+=PixelColor.getGreen();
+               B+=PixelColor.getBlue();
+
+            }
+        }
+        R/=25;
+        G/=25;
+        B/=25;
+        float[] HSV = new float[3];
+        java.awt.Color.RGBtoHSB(R,G,B,HSV);
+        return HSV;
+    }
+
+
+    public Color defineColor(float[] rgb)
+    {
+        float H=rgb[0];
+        float S=rgb[1];
+        float V=rgb[2];
+        if (H >= 0.9f) //OK
         {
             return Color.RED;
         }
-        if (R >= 100 && G >= 60 && B <= 50)
+        if (H < 0.12f)
         {
             return Color.ORANGE;
         }
-        if (R <= 35 && G >= 70 && (B >=30 && B<=80))
+        if ( H < 0.50f && H >=0.30f)
         {
             return Color.GREEN;
         }
-        if (R <=30 && G >= 50 && B >= 110)
+        if (H < 0.90f && H >=0.50f)
         {
-            return Color.BLUE;
+            if (S <= 0.30)
+            {
+                return Color.WHITE;
+            }else{
+                return Color.BLUE;
+            }
+
         }
-        if (R >= 110 && G >= 110 && (B >= 51 && B <=109))
+        if (H >= 0.12f && H < 0.30f)
         {
             return Color.YELLOW;
-        }
-        if (R >= 110 && G >= 110 && B >= 110)
-        {
-            return Color.WHITE;
         }
         return Color.BLACK;
 
